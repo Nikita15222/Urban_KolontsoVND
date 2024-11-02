ae76674c297a87f753430280493e85bb42ff77cb
Nikita15222 <Nikita15222>
2024-11-02 21:40:29 +0300

    init

--- Модуль 5/module_5_1.py
+++ Модуль 5/module_5_1.py
@@ -4,8 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self,new_floor):
-        self.new_floor = new_floor
-        if self.new_floor < 1 or self.new_floor > self.number_of_floors:
+        if new_floor < 1 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
         else:
             for i in range(1, new_floor+1):
