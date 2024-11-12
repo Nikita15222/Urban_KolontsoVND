60356ddca632d95294c7e0fca1e0b4c5f45f01ca
Nikita15222 <Nikita15222>
2024-11-12 10:31:43 +0300

    init

--- Модуль 5/module_5_1.py
+++ Модуль 5/module_5_1.py
@@ -4,11 +4,6 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self,new_floor):
-        # ранее были строки первого варианта кода
-        # self.new_floor = new_floor
-        # if self.new_floor < 1 or self.new_floor > self.number_of_floors:
-
-        #С учетом оптимизации:
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
         else:
--- Модуль 5/module_5_2.py
+++ Модуль 5/module_5_2.py
@@ -3,6 +3,13 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def go_to(self,new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor+1):
+                print(i)
+    
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
     
